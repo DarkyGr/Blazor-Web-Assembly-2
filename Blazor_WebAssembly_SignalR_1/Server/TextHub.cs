4be5a8e86652932e9ab1fd3c4d4c9d6df5683121
Guillermo Riaño <guilledark17@gmail.com>
2023-07-02 10:20:48 -0600

    ✅ TextHub Class (Send Method)

---
+++ Blazor_WebAssembly_SignalR_1/Server/TextHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Blazor_WebAssembly_SignalR_1.Server
+{
+    public class TextHub : Hub
+    {
+        public async Task Send(string text)
+        {
+            await Clients.All.SendAsync("ReceiveInformation", text);
+        }
+    }
+}
